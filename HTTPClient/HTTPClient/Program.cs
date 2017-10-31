c7486c17dfed186213c088b22a73c13efba48530
DaniilStrunov <infqyth6@gmail.com>
2017-11-01 00:49:02 +0300

    initial

---
+++ HTTPClient/HTTPClient/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+
+namespace HTTPClient
+{
+    public class Human
+    {
+        public string name { get; set; }
+        public string id { get; set; }
+        public string eyecolour { get; set; }
+      
+    }
+     class Program
+    {
+        public static string GetHtmlCode(Uri uri)
+        { 
+                HttpWebRequest proxy_request = (HttpWebRequest)WebRequest.Create(uri);
+                proxy_request.Method = "GET";
+                proxy_request.KeepAlive = true;
+                HttpWebResponse resp = proxy_request.GetResponse() as HttpWebResponse;
+                string html = resp.ToString();
+                using (StreamReader sr = new StreamReader(resp.GetResponseStream(), Encoding.GetEncoding(1251)))
+                html = sr.ReadToEnd();
+                html = html.Trim();
+                return html;
+        }
+        static void Main(string[] args)
+        {
+            Uri uri = new Uri("http://127.0.0.1:8888/id/");
+            Uri uriNEXT = new Uri("http://127.0.0.1:8888/");
+
+            string Json = GetHtmlCode(uri);
+
+            List<Human> listhuman = JsonConvert.DeserializeObject<List<Human>>(Json);
+
+            string Json1 = "[";
+            foreach (Human person in listhuman)
+            {
+                Json1 += GetHtmlCode(new Uri(uriNEXT, person.id));
+                Json1 += ",";
+            }
+            Json1 += "]";
+
+            listhuman = JsonConvert.DeserializeObject<List<Human>>(Json1);
+            foreach (Human person in listhuman)
+            {
+                Console.WriteLine(person.name + " " + person.id + " " + person.eyecolour);
+            }
+            Human girlWithRadEyes = listhuman.Where(p => p.eyecolour == "Rad").First();
+
+            Console.WriteLine("Имя девушки с красными глазами: "+ girlWithRadEyes.name);
+            Console.ReadKey();
+        }      
+    }
+}
---
+++ HTTPServer/HTTPServer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace NetConsoleApp
+{
+    class Program
+    {
+        public static void ShowInfirmaition(HttpListenerContext context, string str)
+        {
+            HttpListenerResponse response = context.Response;
+            string responseStr = str;
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseStr);
+            response.ContentLength64 = buffer.Length;
+            Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            output.Close(); 
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ожидание подключений...");
+            while (true)
+            {
+                HttpListener listener = new HttpListener();
+
+                listener.Prefixes.Add("http://127.0.0.1:8888/id/");
+                listener.Prefixes.Add("http://127.0.0.1:8888/1/");
+                listener.Prefixes.Add("http://127.0.0.1:8888/2/");
+                listener.Prefixes.Add("http://127.0.0.1:8888/3/");
+                listener.Prefixes.Add("http://127.0.0.1:8888/4/");
+                listener.Start();
+                HttpListenerContext context = listener.GetContext();
+                string str = context.Request.Url.ToString();
+                switch (str)
+                {
+                    case "http://127.0.0.1:8888/id/":
+                        ShowInfirmaition(context, @"[{""name"":""Dasha"",""id"":""4/""},{ ""name"":""Natasha"",""id"":""2/""},{""name"":""Katya"", ""id"":""1/""},{""name"":""Nastya"",""id"":""3/""}]");
+                        listener.Close();
+                        break;
+                    case "http://127.0.0.1:8888/1/":
+                        ShowInfirmaition(context, @"{""name"":""Katya"",""id"":""1/"", ""eyeColour"":""Rad""}");
+                        listener.Close();
+                        break;
+                    case "http://127.0.0.1:8888/2/":
+                        ShowInfirmaition(context, @"{""name"":""Natasha"",""id"":""2/"", ""eyeColour"":""Gray""}");
+                        listener.Close();
+                        break;
+                    case "http://127.0.0.1:8888/3/":
+                        ShowInfirmaition(context, @"{""name"":""Nastya"",""id"":""3/"", ""eyeColour"":""Green""}");
+                        listener.Close();
+                        break;
+                    case "http://127.0.0.1:8888/4/":
+                        ShowInfirmaition(context, @"{""name"":""Dasha"",""id"":""4/"", ""eyeColour"":""Blue""}");
+                        listener.Close();
+                        break;
+                }
+            }
+        }
+    }
+}
